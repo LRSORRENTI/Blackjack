98879e4661dc093dda2cfa3ce853feb15383c6cd
Luke <lrsorrenti@hotmail.com>
2024-04-28 09:08:48 -0500

    Added genDeck function setup

--- javascript/genDeck.js
+++ javascript/genDeck.js
@@ -1,7 +1,15 @@
 "use strict";
+// Generate Deck File
 // cards should be object structure: 
 // {card: "king", suit: "clubs"}
+const deck = [];
 const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
     "Jack", "Queen", "King", "Ace"];
 const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-const deck = [];
+for (const card of cards) {
+    for (const suit of suits) {
+        // console.log(card, suit);
+        deck.push({ card: card, suit: suit });
+    }
+}
+console.log(deck);
--- typescript/genDeck.ts
+++ typescript/genDeck.ts
@@ -1,8 +1,16 @@
+// Generate Deck File
+
 // cards should be object structure: 
 // {card: "king", suit: "clubs"}
-
+const deck = [];
 const cards: string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
 "Jack", "Queen", "King", "Ace"];
 const suits: string[] = ["Hearts", "Clubs", "Diamonds", "Spades"];
 
-const deck = [];
+for(const card of cards) {
+    for(const suit of suits) {
+        // console.log(card, suit);
+        deck.push({card: card, suit: suit});
+    }
+}
+console.log(deck)
