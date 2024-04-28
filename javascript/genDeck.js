ee1f8cf9c544a628846950200961f3a390d6b173
Luke <lrsorrenti@hotmail.com>
2024-04-28 08:37:18 -0500

    added cards array and suits array to pull from

---
+++ javascript/genDeck.js
@@ -0,0 +1,7 @@
+"use strict";
+// cards should be object structure: 
+// {card: "king", suit: "clubs"}
+const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+    "Jack", "Queen", "King", "Ace"];
+const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+const deck = [];
---
+++ typescript/genDeck.ts
@@ -0,0 +1,8 @@
+// cards should be object structure: 
+// {card: "king", suit: "clubs"}
+
+const cards: string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+"Jack", "Queen", "King", "Ace"];
+const suits: string[] = ["Hearts", "Clubs", "Diamonds", "Spades"];
+
+const deck = [];
