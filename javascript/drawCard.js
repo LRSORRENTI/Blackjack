5048d10e02300f80b30664e0eaebb554eeba8c21
Luke <lrsorrenti@hotmail.com>
2024-04-28 09:33:00 -0500

    Added drawCard function setup

---
+++ javascript/drawCard.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Function to draw single card from generated deck
+const genDeck_1 = require("./genDeck");
+// add way to grab a random card from a deck, maybe 
+// generate a random number between 0 and 51 and then use 
+// that number to grab the index?
+function drawCard() {
+}
+exports.default = drawCard;
+console.log(genDeck_1.deck);
--- javascript/genDeck.js
+++ javascript/genDeck.js
@@ -1,15 +1,20 @@
 "use strict";
 // Generate Deck File
-// cards should be object structure: 
-// {card: "king", suit: "clubs"}
-const deck = [];
-const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-    "Jack", "Queen", "King", "Ace"];
-const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-for (const card of cards) {
-    for (const suit of suits) {
-        // console.log(card, suit);
-        deck.push({ card: card, suit: suit });
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deck = exports.genDeck = void 0;
+function genDeck() {
+    const deck = [];
+    const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "Jack", "Queen", "King", "Ace"];
+    const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+    for (const card of cards) {
+        for (const suit of suits) {
+            // console.log(card, suit);
+            deck.push({ card: card, suit: suit });
+        }
     }
+    return deck;
 }
-console.log(deck);
+exports.genDeck = genDeck;
+const deck = genDeck();
+exports.deck = deck;
