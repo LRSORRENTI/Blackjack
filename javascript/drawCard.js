647d44aed06cb342d160c30eb84a1d3dd39bb941
Luke <lrsorrenti@hotmail.com>
2024-04-28 09:43:50 -0500

    updated tsconfig

--- javascript/drawCard.js
+++ javascript/drawCard.js
@@ -1,11 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// Function to draw single card from generated deck
-const genDeck_1 = require("./genDeck");
 // add way to grab a random card from a deck, maybe 
 // generate a random number between 0 and 51 and then use 
 // that number to grab the index?
 function drawCard() {
 }
 exports.default = drawCard;
-console.log(genDeck_1.deck);
+let getNum = Math.floor(Math.random() * 51);
+console.log(getNum);
+for (let i = 0; i < 1000; i++) {
+    let getNumLoop = Math.floor(Math.random() * 51);
+    if (i === 51) {
+        console.log(i, 'hhoray');
+    }
+    console.log(getNumLoop);
+}
