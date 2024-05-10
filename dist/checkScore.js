5383346bd6102bbde367f0a626902a9bf8cd4e1a
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-05-10 08:06:55 -0500

    changed gitignore

---
+++ dist/checkScore.js
@@ -0,0 +1,25 @@
+import { playerHand, dealerHand } from "./getHands.js";
+function checkScore(hand) {
+    let total = 0;
+    for (const cardObj of hand) {
+        // check if jack, queen or king
+        if (cardObj.card.length > 2 && cardObj.card != 'Ace') {
+            // if length is greater than 2 ie king, queen,
+            // jack then assign 10
+            // console.log(cardObj.card)
+            total += 10;
+        }
+        else if (cardObj.card === 'Ace') {
+            total += 1;
+        }
+        else {
+            total += parseInt(cardObj.card);
+        }
+    }
+    ;
+    console.log(hand);
+    return total;
+}
+console.log('player hand:', checkScore(playerHand));
+console.log('dealer hand:', checkScore(dealerHand));
+export { checkScore };
---
+++ dist/drawCard.js
@@ -0,0 +1,19 @@
+// Function to draw single card from generated deck
+// import { myDeck } from "./genDeck";
+// add way to grab a random card from a deck, maybe 
+// generate a random number between 0 and 51 and then use 
+// that number to grab the index?
+// console.log(myDeck.length)
+export default function drawCard(deck) {
+    // generate random number with max of deck.length which is 52
+    const randomIdx = Math.floor(Math.random() * deck.length);
+    const card = deck[randomIdx];
+    // console.log(deck);
+    // splice() is an important choice because we need to 
+    // alter the length of the array, if we used slice() 
+    // instead it would not modify the length
+    deck.splice(randomIdx, 1);
+    // console.log(randomIdx, card);
+    return card;
+}
+// const myCard = drawCard(myDeck);
